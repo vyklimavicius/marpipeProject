0ae0604cad0b2455ff1504999f38bc2ea0f72a09
vyklimavicius <vyklimavicius@gmail.com>
2019-11-20 15:16:55 -0500

    minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_000049) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "ratings", force: :cascade do |t|
-    t.integer "value"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "specialty"
-    t.integer "rating"
-    t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "restaurant_id", null: false
-    t.string "avatar"
-    t.string "name"
-    t.text "post"
-    t.integer "rating_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["rating_id"], name: "index_reviews_on_rating_id"
-    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "lastname"
-    t.string "password_digest"
-    t.string "email"
-    t.string "avatar"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "reviews", "ratings"
-  add_foreign_key "reviews", "restaurants"
-  add_foreign_key "reviews", "users"
 end
